5168a55dfc6dc296736144ce9cb0b07817f67dc0
Drew Beamer <andrewmbeamer@gmail.com>
2025-05-16 08:35:22 -0400

    initialize attendance app

---
+++ apps/attendance/src/app/api/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import { googleSheetsClient } from "@/lib/google";
+import { z } from "zod";
+
+const RecordSchema = z.object({
+	discordId: z.string(),
+	name: z.string(),
+	timestamp: z.coerce.date(),
+	isCheckingIn: z.coerce.boolean(),
+});
+
+export async function GET(request: NextRequest) {
+	try {
+		const result = await googleSheetsClient.spreadsheets.values.get({
+			spreadsheetId: "1MI2rE8SnRt1TpZmepBvnLSfk2n_eEUWsgyhsEDWfqvw",
+			range: "A:D",
+		});
+		if (!result.data.values) {
+			return NextResponse.json({ error: "No data found" });
+		}
+		const columnIndexMapping = result.data.values[0]?.reduce(
+			(acc, value, index) => {
+				return {
+					...acc,
+					[value]: index,
+				};
+			},
+			{}
+		);
+		if (!columnIndexMapping) {
+			return NextResponse.json({
+				error: "No column index mapping found",
+			});
+		}
+		console.log(columnIndexMapping);
+		const records = result.data.values?.slice(1).map((value) => {
+			return RecordSchema.parse({
+				discordId: value[columnIndexMapping.discordId],
+				name: value[columnIndexMapping.name],
+				timestamp: value[columnIndexMapping.timestamp],
+				isCheckingIn: value[columnIndexMapping.isCheckingIn] === "IN",
+			});
+		});
+		return NextResponse.json(records);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{ error: "Failed to fetch data from Google Sheets" },
+			{ status: 500 }
+		);
+	}
+}
---
+++ apps/attendance/src/lib/google.ts
@@ -0,0 +1,17 @@
+import { google } from "googleapis";
+
+const credentials = JSON.parse(
+	Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS!, "base64").toString(
+		"utf-8"
+	)
+);
+
+const auth = new google.auth.GoogleAuth({
+	credentials,
+	scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+export const googleSheetsClient = google.sheets({
+	version: "v4",
+	auth,
+});
