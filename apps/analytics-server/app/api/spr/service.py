afdd08459ddedd2d8151aad57304f7e070a3ac8b
Drew Beamer <andrewmbeamer@gmail.com>
2025-05-16 08:41:35 -0400

    delete unused apps

--- apps/analytics-server/app/api/spr/service.py
+++
@@ -1,9 +0,0 @@
-class SPRService:
-
-    @staticmethod
-    def get_spr():
-        return {"message": "Hello, World!"}
-
-    @staticmethod
-    def get_scout_spr(team_id: int):
-        return {"message": "Hello, World!"}
