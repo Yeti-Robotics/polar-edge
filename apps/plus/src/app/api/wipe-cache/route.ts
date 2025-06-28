cdc7a04e15020329167abedbea9bffd9d0ca741e
Drew Beamer <andrewmbeamer@gmail.com>
2025-06-27 22:26:48 -0400

    delete plus

--- apps/plus/src/app/api/wipe-cache/route.ts
+++
@@ -1,9 +0,0 @@
-import { revalidateTag } from "next/cache";
-
-export async function GET() {
-	if (process.env.NODE_ENV !== "development") {
-		return new Response("Not allowed", { status: 403 });
-	}
-	revalidateTag("attendance-data");
-	return new Response("Cache wiped", { status: 200 });
-}
