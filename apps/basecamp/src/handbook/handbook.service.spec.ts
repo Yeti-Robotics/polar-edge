e1f80ecac92f918b4461d5ae0b96ce6f5e0c2099
Drew Beamer <andrewmbeamer@gmail.com>
2025-07-01 17:39:29 -0400

    fix tests

--- apps/basecamp/src/handbook/handbook.service.spec.ts
+++ apps/basecamp/src/handbook/handbook.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HandbookService } from './handbook.service';
+import { ConfigService } from '@nestjs/config';
+import { AiService } from '../ai/ai.service';
 
 describe('HandbookService', () => {
   let service: HandbookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HandbookService],
+      providers: [
+        HandbookService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+        {
+          provide: AiService,
+          useValue: {
+            models: {
+              getAiClient: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<HandbookService>(HandbookService);
